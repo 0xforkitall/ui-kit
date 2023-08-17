c328b3e545918dcf2286450e0463762535ee8e0a
0xforkitall <0xforkitall@gmail.com>
2023-08-17 14:03:49 +0200

    Implement new docs package

---
+++ packages/docs/.storybook/main.js
@@ -0,0 +1,34 @@
+import { dirname, join } from 'path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+    return dirname(require.resolve(join(value, 'package.json')));
+}
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+    stories: ['../../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        getAbsolutePath('@storybook/addon-links'),
+        getAbsolutePath('@storybook/addon-essentials'),
+        getAbsolutePath('@storybook/addon-onboarding'),
+        getAbsolutePath('@storybook/addon-interactions'),
+        {
+            name: getAbsolutePath('@storybook/addon-styling'),
+            options: {
+                sass: { implementation: require('sass') },
+            },
+        },
+    ],
+    framework: {
+        name: getAbsolutePath('@storybook/react-webpack5'),
+        options: {},
+    },
+    docs: {
+        autodocs: 'tag',
+    },
+};
+export default config;
---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,17 @@
+import '../../ui-kit-nextjs/styles.scss';
+import '../../ui-kit/styles.scss';
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
+    },
+};
+
+export default preview;
